6be4ef677b9393ab69d283432c0bd705449c464e
srishty.mittal <srishty.mittal@salesforce.com>
2020-11-01 01:31:33 +0530

    Added new test condition

--- app/src/androidTest/java/com/example/ananya/news/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/ananya/news/ExampleInstrumentedTest.java
@@ -21,6 +21,7 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
+        assertNotNull(appContext);
         assertEquals("com.example.ananya.news", appContext.getPackageName());
     }
 }
